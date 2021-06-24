e6eff0ec6beaa7041998a9457772ec6bac49c7ba
Ariel Ramirez DeCoDev <ariel20jona@gmail.com>
2021-06-23 19:01:49 -0600

    cambios en el proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { WebsocketService } from './services/websocket.service.ts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'graficas';
+export class AppComponent implements OnInit {
+
+  public saleData: any;
+  constructor(
+    private http: HttpClient,
+    public _webSocketService: WebsocketService
+  ) { }
+
+
+  ngOnInit(): void {
+    this.getData();
+    this.escucharSocket();
+  }
+
+  getData() {
+    this.http.get('http://localhost:3000/grafica')
+      .subscribe(
+        (resp: any) => {
+          this.saleData = resp
+        }
+      );
+  }
+
+  escucharSocket() {
+    try {
+      this._webSocketService.listen('cambio-grafica')
+        .subscribe(
+          (data: any) => {
+            this.saleData = data;
+          }
+        )
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { NgxChartsModule }from '@swimlane/ngx-charts';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
+import {  HttpClientModule } from '@angular/common/http'
+
+ 
+const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    SocketIoModule.forRoot(config),
+    NgxChartsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
